c04d01eb75dfb0bda15c6b2537874bd1ddb1dfc7
unknown <debkanta.starwaywebdigital@gmail.com>
2026-05-01 18:52:59 +0530

    first commit

---
+++ src/util/config.js
@@ -0,0 +1,9 @@
+const admin = sessionStorage.getItem("isAdmin");
+
+export const baseURL = process.env.REACT_APP_BACKEND_URI
+
+export const fileURL =process.env.REACT_APP_FILE_URI
+
+export const secretKey = process.env.REACT_APP_SECRET_KEY;
+export const projectName = "";
+export const folderStructurePath = "";
